53cf7837898a59e7b028c6c1c9c534ebd9d24ca5
rho <ryan@theoleschool.com>
2014-04-25 00:48:21 -0500

    breaking things out to a more 'angular' structure

--- public/javascripts/app/app.js
+++ public/javascripts/app/app.js
@@ -1,17 +1,63 @@
-angular.module('stayClassyPc', []);
+var app = angular.module('stayClassyPc', []);
 
-angular.module('stayClassyPc').controller('StayClassyListing', ['$scope', '$http', function($scope, $http){
-http://www.stayclassy.org/api1/account-info?cid=13512&token=HOrZfn1emVn9bWpyvojz
-  $http.get('http://www.stayclassy.org/api1/fundraiser-info?cid=13512&token=HOrZfn1emVn9bWpyvojz&eid=31404&fcid=314969')
-    .success(function(data, status, headers, config){
-      $scope.fundraiser = {
-        url: data.fundraiser_url,
-        donationUrl: data.donation_url,
-        eventName: data.event_name,
-        projectName: data.project_name,
-        memberName: data.member_name,
-        memberImg: data.member_image_medium
-      }
-    });
-    console.log($scope);
+app.constant('scAuth', {
+  baseUrl: 'http://www.stayclassy.org/api1/',
+  token: 'HOrZfn1emVn9bWpyvojz',
+  cid: 13512
+});
+
+/**
+ * scOptions
+ *
+ * Value service to hold our query options, pulls straight fundraiser feed by default.
+ * Extend this to add option variables.
+ */
+app.value('scOptions', {
+  queryType: 'fundraisers'
+});
+
+/**
+ * percent
+ *
+ * Quick filter to calculate percentage of donation.
+ */
+app.filter('percent',function() {
+  return function(goal, contributed) {
+    if(goal === 0) {
+      return '';
+    }
+    var percentage = (contributed/goal)*100;
+    percentage.toFixed();
+    return percentage + '%';
+  };
+});
+
+/**
+ * scFetch
+ *
+ * A factory to compile our options service and run out http request Stay Classy.
+ */
+app.factory('scFetch', function($http, scAuth, scOptions) {
+  return {
+    scGet: function() {
+      var scHttpRequest = scAuth.baseUrl + scOptions.queryType + '?cid=' + scAuth.cid + '&token=' + scAuth.token;
+      var promise =
+  $http.get(scHttpRequest).then(function(res) {
+        return res.data;
+      });
+      return promise;
+    }
+  }
+});
+
+/**
+ * ScListingController
+ *
+ * Simple controller to inject our returned fundraisers array to scope.
+ */
+app.controller('ScListingController', ['$scope', 'scFetch', function($scope, scFetch){
+  scFetch.scGet().then(function(scData){
+    $scope.fundraisers = scData.fundraisers;
+  });
+  console.log($scope);
 }]);
